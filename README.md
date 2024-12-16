378364b1a451b33759f36d48eda330c3651cacef
Luc <luc@lucemans.nl>
2024-12-17 00:59:04 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Icons
+
+This repository aims to collect and expose a enumeration of icons that can be used from different sources.
---
+++ src/index.ts
@@ -0,0 +1,18 @@
+// ensure temp directory at ./tmp
+
+// git clone --depth 1 https://github.com/FortAwesome/Font-Awesome.git ./tmp/fa
+
+// inside the directory ["svgs/brands", "svgs/regular", "svgs/solid"]
+// enumerate all files ending with .svg
+
+// output json with format "fa.json"
+// {
+//     "svgs": {
+//         "brands": [
+//             "address-book"
+//         ]
+//     }
+// }
+
+// output to /dist/fa.json
+
